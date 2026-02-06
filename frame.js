6d49f61b9f456c9d206fdefb92a2bd4206005264
Derin Eryilmaz <mathia.is.fun@gmail.com>
2026-02-06 10:54:42 -0500

    basic selectors done

--- frame.js
+++ frame.js
@@ -1,14 +1,35 @@
 const SELECTORS = [
-    [".dd-book-card, .dd-card-container, .ribbon-card-container", elem => {
+    [".dd-book-card, .dd-card-container, .ribbon-card-container, .product-title-details", elem => {
         let container = elem.querySelector(".call-number-container");
         let child = container?.children[1];
+
         return !child ? null : [container, {
             callNumber: child.innerText,
-            name: elem.querySelector(".title-clamp, .title-row")?.innerText,
+            name: elem.querySelector("img")?.alt,
             sublocation: elem.querySelector(".sublocation-container")?.innerText,
             available: !elem.querySelector(".out"),
         }];
     }],
+    [".cdk-dialog-container", elem => {
+        let divs = Array.from(elem.querySelectorAll("div"));
+        let lines = divs.flatMap(e => e.innerText.split("\n"));
+
+        let find = (prefix) => lines
+            .find(l => l.startsWith(prefix))
+            ?.substring(prefix.length);
+
+        let callNumber = find("Call Number: ");
+        if (!callNumber) return null;
+
+        let sublocation = find("Sublocation: ");
+        let name = elem.querySelector(".clickable-book-name, .title-clamp")?.innerText;
+        let available = !elem.querySelector(".out");
+
+        return [
+            elem.querySelector(".channel-board"),
+            { callNumber, sublocation, name, available }
+        ];
+    }]
 ];
 
 
@@ -68,63 +89,6 @@ function addOldButtons() {
     console.log(info);
 
     let prev = document.getElementById("notesSummary");
-    insertButton(prev, info);
-}
-
-function addNewButtons() {
-    let getInfo = (elem) => {
-        let callNumberContainer = elem.querySelector(".call-number-container");
-        if (!callNumberContainer) return null;
-
-        return {
-            callNumber: callNumberContainer.children[1].innerText,
-            name: elem.querySelector(".title-clamp, .title-row")?.innerText,
-            sublocation: elem.querySelector(".sublocation-container")?.innerText,
-            available: !elem.querySelector(".out")
-        };
-    };
-
-    let cards = document.querySelectorAll(".dd-book-card, .dd-card-container, .ribbon-card-container");
-    // image alt is the title
-    for (let card of cards) {
-        if (hasButton(card)) continue;
-
-        let info = getInfo(card);
-        if (!info) continue;
-
-        let prev = card.querySelector(".call-number-container");
-        insertButton(prev, info);
-    }
-
-    let main = document.querySelector(".product-title-details");
-    if (main && !hasButton(main)) {
-        let prev = main.querySelector(".ratings");
-        insertButton(prev, getInfo(main));
-    }
-}
-
-function addSidebarButtons() {
-    let main = document.querySelector(".product-title-details");
-    if (!main || hasButton(main)) return;
-
-    let divs = Array.from(main.querySelectorAll("div"));
-    let lines = divs.flatMap(e => e.innerText.split("\n"));
-
-    let find = (prefix) => lines
-        .find(l => l.startsWith(prefix + ": "))
-        ?.substring(prefix.length + 2);
-
-    let callNumber = find("Call Number");
-    if (!callNumber) return null;
-
-    let sublocation = find("Sublocation");
-    let name = main.querySelector(".clickable-book-name, .title-clamp")?.innerText;
-    let available = !main.querySelector(".out");
-    let info = { callNumber, sublocation, name, available };
-
-    let prev = main.querySelector(".login");
-
-    console.log(info);
     insertButton(prev, info);
 }
 */
